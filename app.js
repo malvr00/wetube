2a191e30c4f09aefefe55d5e089e07c040c867f8
yong <show10_@naver.com>
2021-02-09 23:20:10 +0900

    MVC and Pugfirst

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const morgan = require('morgan')
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
-
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express()
 
-const handleHome = (req, res) => res.send("Hello!");
-
-const handleProfile = (req, res) => res.send("profile");
+app.set('view engine', "pug");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -18,11 +18,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get("/profile", handleProfile);
-
-
-app.use("/user", userRouter);
+//app.use("/user", userRouter);
 
 export default app;
