06ee0f441dbab52e1dede5e801dd8dc557e0404c
yedn1128@naver.com <yedn1128@naver.com>
2021-03-06 23:09:08 +0900

    volum custom

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -5,6 +5,7 @@ const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScreen = document.getElementById("jsFullScreen");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const volumeRange = document.getElementById("jsValume");
 //video mdn elements 사이트 보기 -> htmlMediaelement
 
 //Play Button
@@ -25,7 +26,9 @@ function handleVolumeCilck() {
     // 오디오 음소거 여부를 Boolean 값으로 반환합니다. 음소거라면 true 반대는 false 를 반환합니다..
     videoPlayer.muted = false;
     volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    volumeRange.value = videoPlayer.volume; // 이전 값 기억하고 있음.
   } else {
+    volumeRange.value = 0;
     videoPlayer.muted = true;
     volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
@@ -83,24 +86,49 @@ const formatDate = (seconds) => {
 };
 
 //현제 재생 표시
-function getCurrentTime(){ // 현재 재생 시점을 초 단위로 표현한 double값입니다. 이 값을 세팅하여 재생 시점을 변경할 수 있습니다.
-    currentTime.innerText = formatDate(videoPlayer.currentTime)
-    // 한번말 발생함.
+function getCurrentTime() {
+  // 현재 재생 시점을 초 단위로 표현한 double값입니다. 이 값을 세팅하여 재생 시점을 변경할 수 있습니다.
+  currentTime.innerText = formatDate(Math.floor(videoPlayer.currentTime));
+  // 한번말 발생함.
 }
 
 //전체 동영상 길이
-function setTotalTime(){// 미디어의 전체 길이를 초 단위로 double 값으로 반환합니다. 재생 가능한 미디어가 없을 경우 0을 반환합니다.
-    const totlaTimeString = formatDate(videoPlayer.duration); 
-    totalTime.innerText = totlaTimeString;
-    setInterval(getCurrentTime,1000); // 매 초마다 호출. (Timer 함수)
+function setTotalTime() {
+  // 미디어의 전체 길이를 초 단위로 double 값으로 반환합니다. 재생 가능한 미디어가 없을 경우 0을 반환합니다.
+  const totlaTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerText = totlaTimeString;
+  setInterval(getCurrentTime, 1000); // 매 초마다 호출. (Timer 함수)
 }
 
+//video 종료시 처음 화면으로
+function handleEnded() {
+  videoPlayer.currentTime = 0;
+  playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+}
+
+//볼륨
+function handelDrag(event) {
+  const {
+    target: { value },
+  } = event;
+  videoPlayer.value = value;
+  if(value>=0.6){
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else if(value >=0.2){
+    volumeBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+  } else {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>';
+  }
+}
 
 function init() {
+  videoPlayer.volume = 0.5;
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeCilck);
   fullScreen.addEventListener("click", goFullScreen);
-  videoPlayer.addEventListener("loadedmetadata",setTotalTime); // 데이터가로드 된 경우 이벤트가 발생합니다.
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime); // 데이터가로드 된 경우 이벤트가 발생합니다.
+  videoPlayer.addEventListener("ended", handleEnded); // video 끝났을때
+  volumeRange.addEventListener("input", handelDrag);
 }
 
 if (videoContainer) {
