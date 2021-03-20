25160c9e255437eede3c0d50828162cfffc38192
yedn1128@naver.com <yedn1128@naver.com>
2021-03-20 17:17:46 +0900

    test

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, 
+mongoose.connect(process.env.PRODUCTION ? process.env.MONGO_URL : process.env.MONGO_URL, 
  {
    useNewUrlParser: true,
    //userFindAndModify: false,
