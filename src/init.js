e52f6435fc86e49145af0d519e1c86d9e87abdd8
yedn1128@naver.com <yedn1128@naver.com>
2021-03-20 17:40:14 +0900

    host 수정

--- src/init.js
+++ src/init.js
@@ -8,9 +8,9 @@ import "./models/video";
 import "./models/comment";
 import "./models/user";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PRODUCTION ? `http://localhost:${process.env.PORT}` : `https://rocky-chamber-06274.herokuapp.com/`;
 
 const handleListening = () =>
-  console.log(`✔ Listening on: http://localhost:${PORT}`);
+  console.log(`✔ Listening on: ${PORT}`);
 
 app.listen(PORT, handleListening);
